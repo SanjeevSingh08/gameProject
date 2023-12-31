0ca205176f19fd21cb5b37bce8cb59352c4023e9
Sanjeev Ranjan Singh <100428475+SanjeevSingh08@users.noreply.github.com>
2023-12-31 11:35:07 +0530

    commit

--- src/app/component/Navbar.jsx
+++ src/app/component/Navbar.jsx
@@ -1,11 +1,11 @@
-'use client';
-import React, { useEffect } from 'react';
+
+
 import logo from '../../../public/tc_new.png';
 import Image from 'next/image';
 import p3 from '../../../public/p3.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDown, faCalendar } from '@fortawesome/free-solid-svg-icons';
-
+import logo1 from '../../../public/logo1.svg';
 import feature from '../../../public/feature.png';
 import p2 from '../../../public/p2.png';
 import p5 from '../../../public/p5.png';
@@ -30,37 +30,11 @@ import tata from '../../../public/tata.png';
 import brain from '../../../public/brain.png';
 
 const Navbar = () => {
-  // useEffect(() => {
-  //   let prevScrollPos = window.scrollY || window.pageYOffset;
-
-  //   const handleScroll = () => {
-  //     const currentScrollPos = window.scrollY || window.pageYOffset;
-  //     const navbar = document.querySelector('.nav_section');
-
-  //     if (prevScrollPos > currentScrollPos || currentScrollPos < 10) {
-  //       // Scroll up or close to the top
-  //       navbar.classList.add('visible');
-  //       navbar.classList.remove('hidden');
-  //     } else {
-  //       // Scroll down
-  //       navbar.classList.remove('visible');
-  //       navbar.classList.add('hidden');
-  //     }
-
-  //     prevScrollPos = currentScrollPos;
-  //   };
-
-  //   window.addEventListener('scroll', handleScroll);
-
-  //   return () => {
-  //     window.removeEventListener('scroll', handleScroll);
-  //   };
-  // }, []);
-
+  
   return (
     <>
-      <section className='nav_section visible'>
-        <Image src={logo} alt='logo' width={74} height={48} />
+      <section className='nav_section '>
+        <Image src={logo1} alt='logo' width={74} height={48} />
       </section>
 
       <section className='hero_section section'>
